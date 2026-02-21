06da8bdcb232d48b18378c2d7f53e89d72ffc457
Rupesh Lakshman <codewithruppu@gmail.com>
2026-02-21 07:13:13 +0530

    port 8080

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/health", (req, res) => {
 	res.json({ status: "ok", app: APP_NAME });
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
 	console.log("Server running");
 });
