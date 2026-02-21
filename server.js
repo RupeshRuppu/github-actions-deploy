3494bba714cb25a12e517b4564360b08a20ca478
Rupesh Lakshman <codewithruppu@gmail.com>
2026-02-21 07:02:07 +0530

    port changed

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/health", (req, res) => {
 	res.json({ status: "ok", app: APP_NAME });
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
